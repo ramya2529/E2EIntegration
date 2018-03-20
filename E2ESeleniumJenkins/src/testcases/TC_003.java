c95d27bd018bb13cd0e59fb27e2ed9ce91fdb01c
ramya <ramya@DESKTOP-G814VIJ>
2018-03-20 16:54:40 -0500

    Created Automation Project

---
+++ E2ESeleniumJenkins/src/testcases/TC_003.java
@@ -0,0 +1,20 @@
+package testcases;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.testng.annotations.Test;
+
+public class TC_003 {
+@Test
+public void testcase1()
+{
+	System.setProperty("webdriver.chrome.driver", "./Driver/chromedriver.exe");
+	ChromeOptions options=new ChromeOptions();
+	options.addArguments("--disable-extensions");
+	ChromeDriver driver=new ChromeDriver();
+	driver.get("http://www.facebook.com");
+	driver.findElementById("email").sendKeys("hello");
+	driver.findElementById("pass").sendKeys("hello");
+	driver.quit();
+}
+}
